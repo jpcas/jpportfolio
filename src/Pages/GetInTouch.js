b9628e8dcd6d89cc71569fc5f01cb18e661f969f
JP Castrillon <jpcas001@gmail.com>
2021-02-24 15:49:59 -0500

    added form labels and removed forms not being used

--- src/Pages/GetInTouch.js
+++ src/Pages/GetInTouch.js
@@ -20,50 +20,27 @@ function GetInTouch() {
       <Form>
   <Form.Row>
     <Form.Group as={Col} controlId="formGridEmail">
-      <Form.Label>Email</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Label>First Name</Form.Label>
+      <Form.Control type="email" placeholder="First Name" />
     </Form.Group>
 
     <Form.Group as={Col} controlId="formGridPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
+      <Form.Label>Last Name</Form.Label>
+      <Form.Control type="password" placeholder="Last Name" />
     </Form.Group>
   </Form.Row>
 
   <Form.Group controlId="formGridAddress1">
-    <Form.Label>Address</Form.Label>
-    <Form.Control placeholder="1234 Main St" />
+    <Form.Label>Email</Form.Label>
+    <Form.Control placeholder="Email Address" />
   </Form.Group>
 
   <Form.Group controlId="formGridAddress2">
-    <Form.Label>Address 2</Form.Label>
-    <Form.Control placeholder="Apartment, studio, or floor" />
+    <Form.Label>Comments</Form.Label>
+    <Form.Control placeholder="Leave your comments here!" />
   </Form.Group>
 
-  <Form.Row>
-    <Form.Group as={Col} controlId="formGridCity">
-      <Form.Label>City</Form.Label>
-      <Form.Control />
-    </Form.Group>
-
-    <Form.Group as={Col} controlId="formGridState">
-      <Form.Label>State</Form.Label>
-      <Form.Control as="select" defaultValue="Choose...">
-        <option>Choose...</option>
-        <option>...</option>
-      </Form.Control>
-    </Form.Group>
-
-    <Form.Group as={Col} controlId="formGridZip">
-      <Form.Label>Zip</Form.Label>
-      <Form.Control />
-    </Form.Group>
-  </Form.Row>
-
-  <Form.Group id="formGridCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-
+  
   <Button variant="primary" type="submit">
     Submit
   </Button>
